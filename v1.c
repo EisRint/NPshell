62109d31e5eec5aacafd066e356ce459da41cb7b
da21510 <da21510@gmail.com>
2020-11-07 17:11:25 +0800

    pipe complete

--- v1.c
+++ v1.c
@@ -17,13 +17,18 @@ int main(void) {
 		int flag=0;
 
 		char input[512];
-		
+
 		char Q[7500][512];
 
 		printf("%% ");
 		while(flag==0){
-			input[i]=fgetc(stdin);
-
+			if (feof(stdin)){
+				printf("\n");
+				exit(0);
+			}
+			else{
+				input[i]=fgetc(stdin);
+			}
 
 			if (input[i]==' ' && i!=0 && input[i-1]!=' '){
 				i++;
@@ -47,63 +52,76 @@ int main(void) {
 		}
 
 		//-----------------------------------------------------------
-	int is_empty=0;
-	int l=0;
-	int pipefd0[2];
-	int pipefd1[2];
 
-	while(is_empty==0){		
-		int len;
-		char buffer[1024]={0};
+		if(strcmp(Q[0],"exit")==0){
+			exit(0);
+		}
+
+		int is_empty=0;
+		int l=0;
+		int pipefd0[2];
+		int pipefd1[2];
 
 		if(pipe(pipefd0)<0)
 			printf("pipe1 create error");
-		signal(SIGCHLD,SIG_IGN);
-		if(fork() == 0){ /*child process*/
-			close(pipefd0[0]);
-			dup2(pipefd0[1], STDOUT_FILENO); /*pipefd0 will close after STDIN receive EOF*/
+		while(is_empty==0){		
+			int len;
+			char buffer[1024]={0};
 
-			char *argv[50];
-			int argvi = 0;
-			int it = 0;
-			_Bool f = 1;
-			while(Q[l][it] != '\0') {
-				if(Q[l][it] != ' ') {
-					if (f){
-						argv[argvi] = &Q[l][it];
-						++argvi;
-						++it;
-						f = 0;
+			if(pipe(pipefd1)<0)
+				printf("pipe1 create error");
+			signal(SIGCHLD,SIG_IGN);
+			if(fork() == 0){ /*child process*/
+
+				dup2(pipefd0[0], STDIN_FILENO);
+				close(pipefd1[0]);
+				dup2(pipefd1[1], STDOUT_FILENO); /*pipefd0 will close after STDIN receive EOF*/
+
+				char *argv[50];
+				int argvi = 0;
+				int it = 0;
+				_Bool f = 1;
+				while(Q[l][it] != '\0') {
+					if(Q[l][it] != ' ') {
+						if (f){
+							argv[argvi] = &Q[l][it];
+							++argvi;
+							++it;
+							f = 0;
+						} else {
+							++it;
+						}
 					} else {
+						Q[l][it] = '\0';
 						++it;
+						f = 1;
 					}
-				} else {
-					Q[l][it] = '\0';
-					++it;
-					f = 1;
+				}
+				argv[argvi] = NULL;
+
+
+				execvp(argv[0],argv);
+				exit(0);
+			}else{ /*parent process*/
+				close(pipefd1[1]);
+				close(pipefd0[0]);
+				pipefd0[0]=pipefd1[0];
+
+
+			}
+
+
+			l++;
+			if(l==k){
+				is_empty=1;
+				while((len=read(pipefd1[0],buffer,1023))>0){
+					buffer[len]='\0';
+					printf("%s",buffer);
 				}
 			}
-			argv[argvi] = NULL;
 
 
-			execvp(argv[0],argv);
-			exit(0);
-		}else{ /*parent process*/
-			close(pipefd0[1]);
-			while((len=read(pipefd0[0],buffer,1023))>0){
-				buffer[len]='\0';
-				printf("%s\n",buffer);
-			}
 		}
-
-
-		l++;
-		if(l==k){
-			is_empty=1;
-		}
-
-
-	}
 	}
 
 
